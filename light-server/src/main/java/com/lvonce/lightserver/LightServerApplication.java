00f998d61486f087b019e94920a1c0871229aad5
Wang Wei <thegenius@vip.qq.com>
2018-08-02 22:43:33 +0800

    first commit

---
+++ light-server/src/main/java/com/lvonce/lightserver/LightServerApplication.java
@@ -0,0 +1,12 @@
+package com.lvonce.lightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightServerApplication.class, args);
+    }
+}
